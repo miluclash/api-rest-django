845c2b217250de00ded17df0e9653a4628d0a27c
JA <42705473+fnbutton@users.noreply.github.com>
2026-03-09 12:17:07 +0100

    Add API view functions for Andrea, Michael, and Jonathan with JSON response

--- api/views.py
+++ api/views.py
@@ -1,3 +1,52 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def api_view_andrea(request):
+    # Mchael - frontend
+    # 1 Wikidata
+
+    # Jonathana
+    # 1 PlacesNew
+
+    # Handrea
+    # 1 Clima
+    # 2 Pokemon
+
+    return HttpResponse(json.dumps({
+        "message": "Hello, World!"
+    }), content_type="application/json")
+
+def api_view_michael(request):
+    # Mchael    
+    # 1 Wikidata
+
+    # Jonathan
+    # 1 PlacesNew
+
+    # Handrea
+    # 1 Clima
+    # 2 Pokemon
+
+    return HttpResponse(json.dumps({
+        "message": "Hello, World!"
+    }), content_type="application/json")
+
+def api_view_jonathan(request):
+    # Mchael    
+    # 1 Wikidata
+
+    # Jonathana
+    # 1 PlacesNew
+
+    # Handrea
+    # 1 Clima
+    # 2 Pokemon
+
+    return HttpResponse(json.dumps({
+        "message": "Hello, World!"
+    }), content_type="application/json")
