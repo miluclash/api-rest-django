3850928650d221dab83f67a8a70f42e2be285137
JA <42705473+fnbutton@users.noreply.github.com>
2026-03-11 12:18:38 +0100

    Add api_view_jonathan with crime data retrieval and update response format

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     # Aquí ir añadiendo los endpoints.
+    path('jonathan/', views.api_view_jonathan, name='api_view_jonathan'),
     
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
+import datetime
 import json
-
+from external_servicies.wikidata import wikidata_crime
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
 # Create your views here.
 
 
@@ -36,6 +37,8 @@ def api_view_michael(request):
         "message": "Hello, World!"
     }), content_type="application/json")
 
+
+@api_view(['GET'])
 def api_view_jonathan(request):
     # Mchael    
     # 1 Wikidata
@@ -47,6 +50,12 @@ def api_view_jonathan(request):
     # 1 Clima
     # 2 Pokemon
 
+    
+    searching_crime= wikidata_crime('PruebaCrimenDjango/1.1 (aticasmia007@gmail.com)')
+    today = datetime.datetime.now()
+    crime = searching_crime.get_crime(today.strftime("%Y-%m-%d"))
+
     return HttpResponse(json.dumps({
-        "message": "Hello, World!"
+        "code": 200,
+        "msg": f'{{weather.city}}, {{weather.temp}}°C, {{weather.desc}}. Un {{pokemon.name}} tipo {{pokemon.types}} con las manos sucias y "{crime['itemLabel']}" en el expediente. Nadie pregunta, nadie responde. {{restaurant.name}} sirve cocina {{restaurant.cuisine}} hasta las 11pm. Suficiente tiempo para olvidar todo.'
     }), content_type="application/json")
