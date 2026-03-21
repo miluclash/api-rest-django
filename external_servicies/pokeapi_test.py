e5f99da5257b77c4c860f9cee372cf531b3249e3
Kevin VF <knvfons20@gmail.com>
2026-03-21 15:48:49 +0100

    Feature: Add numPkemons parameter in obtener_pokemon_por_tipo method to return one or more pokemons

--- external_servicies/pokeapi_test.py
+++ external_servicies/pokeapi_test.py
@@ -15,15 +15,29 @@ class PokeAPITest:
 
     def test_obtener_pokemon(self):
         api = PokeAPI()
-        pokemon = api.seleccionar_pokemon(15,5,500,1)
 
+        # caso 1: un solo Pokémon
+        pokemon = api.obtener_pokemon_por_tipo("fire")
         assert isinstance(pokemon, dict)
 
-        required_fields = ["name","types"]
+        required_fields = ["name", "types"]
         for field in required_fields:
             assert field in pokemon
 
-        print("Pokemon válido")
+        print("obtener_pokemon_por_tipo (1 pokemon) OK")
+
+        # caso 2: varios Pokémon
+        pokemons = api.obtener_pokemon_por_tipo("fire", 3)
+        assert isinstance(pokemons, list)
+        assert len(pokemons) == 3
+
+        for p in pokemons:
+            assert isinstance(p, dict)
+            for field in required_fields:
+                assert field in p
+
+        print("obtener_pokemon_por_tipo (varios pokemons) OK")
+
     def test_tipo_invalido(self):
         api = PokeAPI()
 
@@ -31,7 +45,17 @@ class PokeAPITest:
             api.obtener_pokemon_por_tipo("tipoinventado")
             assert False, "Debía lanzar error"
         except ValueError:
-            print("tipo inválido detectado")
+            print("tipo inválido detectado OK")
+
+    def test_obtener_muchos_pokemons(self):
+        api = PokeAPI()
+
+        pokemons = api.obtener_pokemon_por_tipo("fire", 105)
+
+        assert isinstance(pokemons, list)
+        assert len(pokemons) > 0 
+        print("obtener_pokemon_por_tipo maneja exceso de cantidad OK")
+
 
     def run_all(self):
 
@@ -39,7 +63,8 @@ class PokeAPITest:
             self.test_calcular_pesos,
             self.test_seleccionar_tipo,
             self.test_obtener_pokemon,
-            self.test_tipo_invalido
+            self.test_tipo_invalido,
+            self.test_obtener_muchos_pokemons
         ]
 
         for test in tests:
@@ -55,5 +80,11 @@ if __name__ == "__main__":
     tester = PokeAPITest()
     tester.run_all()
     api = PokeAPI()
-    pokemon = api.seleccionar_pokemon(15, 5, 500, 1)
-    print(pokemon)
+    # pokemon = api.seleccionar_pokemon(15, 5, 500, 1)
+    # print(pokemon)
+    # pokemones = api.obtener_pokemon_por_tipo("water", 10)
+    # for p in pokemones:
+    #     print(p["name"])
+    # pokemones = api.obtener_pokemon_por_tipo("water", 1)
+    # print("-----------------")
+    # print(pokemones["name"])
